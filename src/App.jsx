5626a735e7560a89868161ec62dd13bed156b409
King Ahmad <kingahmadilyas03@gmail.comdn>
2025-09-09 00:48:08 +0800

    update the .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
 
 // landing page
-import Front_Page from './Landing_Page/Main_Page.jsx';
-import Front_Index from './Landing_Page/Index.jsx';
-import Front_Why_Us from './Landing_Page/Why.jsx';
-import Front_Features from './Landing_Page/Features.jsx';
-import Front_Faq from './Landing_Page/Faq.jsx';
+import Front_Page from './views/Landing_Page/Main_Page.jsx';
+import Front_Index from './views/Landing_Page/Index.jsx';
+import Front_Why_Us from './views/Landing_Page/Why.jsx';
+import Front_Features from './views/Landing_Page/Features.jsx';
+import Front_Faq from './views/Landing_Page/Faq.jsx';
 
 // login page
-import Login_Page from './Login_Page/Main_Login.jsx';
-import Sign_In from './Login_Page/Sign_In.jsx';
+import Login_Page from './views/Login_Page/Main_Login.jsx';
+import Sign_In from './views/Login_Page/Sign_In.jsx';
 
 // main Apps
-import Notes_App from './Main_Apps/Notes_App.jsx';
+import Notes_App from './views/Main_Apps/Notes_App.jsx';
 
 
 function App() {
@@ -28,7 +28,7 @@ function App() {
       ]
     },
     { path: '/login', element: <Login_Page/>}, // TODO: missing logic
-    {path: '/signin', element: <Sign_In/>}, // sign in page (ui logic)
+    { path: '/signin', element: <Sign_In/>}, // sign in page (ui logic)
     { path: '/notes', element: <Notes_App /> }, // TODO: add some functionality
     { path: '/*', element: <h1 className='pt-10 text-center'>this address goes nowhere, click <Link to='/' className='font-black'>Here</Link> to the landing page</h1> } //TODO: add UI
   ])
--- src/views/Login_Page/Main_Login.jsx
+++ src/views/Login_Page/Main_Login.jsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default () => {
+    const navigate = useNavigate();
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="flex flex-col justify-center items-center w-[450px] border-2 border-transparent rounded-xl py-6 px-8 font-mono [box-shadow:0px_0px_50px_#c7c7c7]">
@@ -8,7 +9,7 @@ export default () => {
                     <h1 className="text-4xl font-extrabold">Welcome back!</h1>
                     <p className="text-gray-600 mt-3">Log in to your Dotify account to continue</p>
                 </div>
-                
+
                 {/* google login section */}
                 <div className="flex justify-center items-center border-2 border-gray-300 rounded-lg py-2 w-full mt-8 hover:bg-stone-200 cursor-pointer transition-color duration-75 ease-in-out">
                     <i className="fa-brands fa-google text-red-500 mr-3"></i>
@@ -38,7 +39,7 @@ export default () => {
                     </div>
 
                     {/* login button */}
-                    <button className="w-full border-2 border-transparent bg-blue-600 rounded-lg py-2 mt-5 text-white font-[800]">Log in</button>
+                    <button onClick={() => navigate('/notes')} className="w-full border-2 border-transparent bg-blue-600 rounded-lg py-2 mt-5 text-white font-[800]">Log in</button>
                 </div>
 
                 {/* sign up section */}
