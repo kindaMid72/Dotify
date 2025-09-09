d4fdd4c3808d08e87aefefb2c2cc8c347d84f6e3
King Ahmad <kingahmadilyas03@gmail.comdn>
2025-09-09 11:01:19 +0800

    add database configuration

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+
+dotenv.config(); // env configuration
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    port: Number(process.env.MYSQL_PORT),
+    database: process.env.MYSQL_DATABASE
+})
+
+export default pool; // pass connection agar bisa digunakan
