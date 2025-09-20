483c0c925b30e122bfde5bef6333c82a744b2a0d
King Ahmad <kingahmadilyas03@gmail.comdn>
2025-09-20 18:05:22 +0800

    notesModels is kinda complete, idk tho

---
+++ src/models/notesModel.js
@@ -0,0 +1,136 @@
+import db from '../config/database.js';
+
+async function createNote({ userId, title, content, is_favorite = 0, is_archived = 0 }) {
+    const sql = `
+        INSERT INTO notes (user_id, title, content, is_favorite, is_archived) 
+        VALUES (?, ?, ?, ?, ?)
+    `;
+    try {
+        const [result] = await db.query(sql, [userId, title, content, is_favorite, is_archived]);
+        return result.insertId;
+    } catch (err) {
+        console.error(err);
+        throw new Error('Gagal membuat catatan: ' + err.message);
+    }
+}
+
+async function deleteNote({ noteId }) {
+    const sql = "DELETE FROM notes WHERE id = ?";
+    try {
+        const [result] = await db.query(sql, [noteId]);
+        return result.affectedRows > 0;
+    } catch (err) {
+        console.error(err);
+        throw new Error('Gagal menghapus catatan: ' + err.message);
+    }
+
+}
+async function editTitle(params) {
+    // params: notesId
+    const noteId = params.noteId;
+    const newTitle = params.title;
+
+    const sql = "UPDATE notes SET title = ? WHERE id = ?";
+    try {
+        const [result] = await db.query(sql, [newTitle, noteId]);
+        updateLastEdited(noteId);
+        return result.affectedRows > 0;
+    } catch (err) {
+        console.error(err);
+        throw new Error('Gagal mengupdate catatan: ' + err.message);
+    }
+}
+async function editContent(params) {
+    // params: notesId
+    const noteId = params.noteId;
+    const newContent = params.content;
+
+    const sql = "UPDATE notes SET content = ? WHERE id = ?";
+    try {
+        const [result] = await db.query(sql, [newContent, noteId]);
+        updateLastEdited(noteId);
+        return result.affectedRows > 0;
+    } catch (err) {
+        console.error(err);
+        throw new Error("Gagal mengupdate catatan: " + err.message);
+    }
+}
+async function setFavorite(params) {
+    // params: notesId
+    const noteId = params.noteId;
+    const isFavorite = params.isFavorite;
+
+    const sql = "UPDATE notes SET is_favorite = ? WHERE id = ?";
+    try {
+        const [result] = await db.query(sql, [isFavorite, noteId]);
+        updateLastEdited(noteId);
+        return result.affectedRows > 0;
+
+    } catch (err) {
+        console.error(err);
+        throw new Error("Gagal mengupdate catatan: " + err.message);
+    }
+}
+async function setArchive(params) {
+    // params: note id
+    const noteId = params.noteId;
+    const isArchive = params.isArchive; // boolean
+
+    const sql = "UPDATE notes SET is_archive = ? WHERE id = ?";
+    try {
+        const [result] = await db.query(sql, [isArchive, noteId]);
+        updateLastEdited(noteId);
+        return result.affectedRows > 0;
+    } catch (err) {
+        console.error(err);
+        throw new Error('Gagal mengambil ringkasan catatan: ' + err.message);
+    }
+}
+async function updateLastEdited(params) {
+    const noteId = params.noteId;
+    const updated_at = Date.now();
+    const sql = "UPDATE notes SET updated_at = ? WHERE id = ?";
+    try {
+        const [result] = await db.query(sql, [updated_at, noteId]);
+        if (result.affectedRows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (err) {
+        console.error(err);
+        return false;
+    }
+}
+
+// return list of notes information
+async function getAllNotesInfo({ userId }) {
+    const sql = "SELECT title, content, is_favorite, is_archived FROM notes WHERE user_id = ?";
+    try{
+        const [result] = await db.query(sql, [userId]);
+        return result; // return all rows
+    }catch(err){
+        throw new Error("Gagal mengambil ringkasan catatan: " + err.message);
+    }
+}
+async function getNoteContent({noteId}) {
+    const sql = "SELECT content FROM notes WHERE id = ?";
+    try{
+        const [result] = await db.query(sql, [noteId]);
+        return result;
+    }catch(err){
+        throw new Error("Gagal mengambil ringkasan catatan: " + err.message);
+    }
+}
+
+export default { 
+    createNote, 
+    deleteNote, 
+    editTitle, 
+    editContent, 
+    setFavorite, 
+    setArchive, 
+    updateLastEdited, 
+    getAllNotesInfo, 
+    getNoteContent 
+};
