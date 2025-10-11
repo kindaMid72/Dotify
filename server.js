b2d8953b5de932d5fd268adaf6b5e9695623801d
King Ahmad <kingahmadilyas03@gmail.comdn>
2025-10-12 07:08:40 +0800

    try deploy 2

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import controllerRoute from './src/controllers/controllerRoute.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000; // default value for development process
+
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000', process.env.FRONTEND_URL];
+
+// Dapatkan __dirname di ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors({
     origin: 'http://localhost:5173', // allow request only from this domain
@@ -19,6 +28,14 @@ app.use(express.urlencoded({ extended: true }));
 // Beritahu Express: Untuk setiap permintaan yang diawali dengan '/api/users',
 // serahkan penanganannya ke `userRoutes`.
 app.use('/api/db', controllerRoute);
+
+// Sajikan file statis dari build React (Vite)
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Untuk semua permintaan GET lainnya yang bukan API, kirim index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 
 app.listen(PORT, () => {
