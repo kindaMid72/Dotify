daa7a68bd062b4b09d20e4aa9a7bb78d91ae2c10
King Ahmad <kingahmadilyas03@gmail.comdn>
2025-10-11 20:49:14 +0800

    one more

--- server.js
+++ server.js
@@ -2,17 +2,11 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import controllerRoute from './src/controllers/controllerRoute.js';
+import controllerRoute from './rc/controllers/controllerRoute.js';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000; // default value for development process
-
-// ES module equivalent of __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Daftar origin yang diizinkan
 const allowedOrigins = ['http://localhost:5173', process.env.FRONTEND_URL]; // Tambahkan URL frontend produksi Anda dari env
@@ -28,17 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 // Beritahu Express: Untuk setiap permintaan yang diawali dengan '/api/users',
 // serahkan penanganannya ke `userRoutes`.
 app.use('/api/db', controllerRoute);
-
-// ---- Konfigurasi untuk Produksi ----
-if (process.env.NODE_ENV === 'production') {
-    // Sajikan file statis dari folder 'dist'
-    app.use(express.static(path.join(__dirname, 'dist')));
-
-    // Untuk semua permintaan GET lainnya, kirim file index.html dari React
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-    });
-}
 
 app.listen(PORT, () => {
     console.log(`Server is up and running or port ${PORT}`);
